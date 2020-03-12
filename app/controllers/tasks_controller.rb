e116ea5f16f6cf12215d6459f472b61d668161e5
田中 洋輝 <1112114@PC0546.local>
2020-03-12 12:20:03 +0900

    新規登録・編集機能追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,5 +6,26 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
-  
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    task = params[:task].permit(:title, :description)
+    Task.create(task)
+    redirect_to root_path, notice: "タスク#{task.title}を登録しました"
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    task = Task.find(params[:id])
+    task.update(params[:task].permit(:title, :description))
+    redirect_to root_path, notice: "タスク#{task.title}を編集しました"
+  end
+
+
 end
