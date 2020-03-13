6964ffcf2d4e009d3930a0c69870d3db9685a32b
田中 洋輝 <1112114@PC0546.local>
2020-03-13 10:40:29 +0900

    タイムゾーンを日本に設定

--- config/application.rb
+++ config/application.rb
@@ -26,12 +26,15 @@ module App
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     
 
     config.i18n.default_locale = :ja
     
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
-    
+
     # Don't generate system test files.
     config.generators.system_tests = nil
     # 自動的にslimファイルを作成されるようにする
