edcb3075e7377843e9743b1a17e83b9b5c02a196
田中 洋輝 <1112114@PC0546.local>
2020-03-17 12:07:36 +0900

    WIP

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -2,11 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Task, type: :system do
   before do
-    build(:task)
+    @task = build(:task)
   end
-  
-  it 'タスク' do
-    task = create(:task)
-    expect(task).to be_valid
+
+  it "タスク一覧の降順確認" do
+    @task = build(:task)
+    expect(@task).to be_valid
   end
+
 end
