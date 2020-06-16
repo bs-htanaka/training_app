a14a88307ba3c4d609b50849c15f56d16461af3a
bs-htanaka <h.tanaka@buysell-technologies.com>
2020-06-16 17:55:53 +0900

    priorityのviewを追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,5 @@ class Task < ApplicationRecord
     validates :title, presence: true
     validates :description, presence: true, length: { maximum: 75 }
     enum status: {未着手: 0, 進行中: 1, 完了: 2}
+    enum priority: {低: 0, 中: 1, 高: 2}
 end
