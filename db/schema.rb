2d98ab23bd5882e64401654a7d702f712bf7cbc9
bs-htanaka <h.tanaka@buysell-technologies.com>
2020-06-17 13:24:58 +0900

    user_idの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_16_160811) do
+ActiveRecord::Schema.define(version: 2020_06_17_042247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2020_06_16_160811) do
     t.datetime "limit_day"
     t.integer "status"
     t.integer "priority"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
