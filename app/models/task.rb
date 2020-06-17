cffd484d7de9a49754bcf4a788f17e4e8b0bea7a
bs-htanaka <h.tanaka@buysell-technologies.com>
2020-06-17 13:26:33 +0900

    ユーザーとタスクの紐付け

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,8 @@
 class Task < ApplicationRecord
-    validates :title, presence: true
-    validates :description, presence: true, length: { maximum: 75 }
-    enum status: {未着手: 0, 進行中: 1, 完了: 2}
-    enum priority: {低: 0, 中: 1, 高: 2}
+  belongs_to :user
+
+  validates :title, presence: true
+  validates :description, presence: true, length: { maximum: 75 }
+  enum status: {未着手: 0, 進行中: 1, 完了: 2}
+  enum priority: {低: 0, 中: 1, 高: 2}
 end
