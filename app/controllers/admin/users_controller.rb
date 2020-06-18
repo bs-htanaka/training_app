45dc8a03395dbb3f8c3f273e2b466e0d39883ae3
bs-htanaka <h.tanaka@buysell-technologies.com>
2020-06-18 15:01:38 +0900

    userの削除

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -35,6 +35,12 @@ class Admin::UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_path, notice: "ユーザー「#{@user.name}」を削除しました"
+  end 
+
 private
   
   def user_params
