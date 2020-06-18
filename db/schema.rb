23a9de4da25c799b8bb310615c261925daa4eefe
bs-htanaka <h.tanaka@buysell-technologies.com>
2020-06-18 15:53:42 +0900

    管理者ロールの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_17_042247) do
+ActiveRecord::Schema.define(version: 2020_06_18_064739) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2020_06_17_042247) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin"
   end
 
 end
