76edb2882b40ead966afbc5cd28a7dcd2211a7f1
bs-htanaka <h.tanaka@buysell-technologies.com>
2020-06-18 13:29:48 +0900

    ユーザ新規登録

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,5 +3,24 @@ class Admin::UsersController < ApplicationController
   def index
     @users = User.all.order(created_at: "DESC")
   end
-    
+  
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_users_path, notice: "ユーザー「#{@user.name}」を登録しました"
+    else
+      render :new
+    end
+  end
+
+private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
 end
