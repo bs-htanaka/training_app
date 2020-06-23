1e89aead34de01ebd3c32339ea6f38dc71cfc2d0
bs-htanaka <h.tanaka@buysell-technologies.com>
2020-06-23 18:35:22 +0900

    エラーページの設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,16 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
   before_action :login_required
 
+  class Forbidden < ActionController::ActionControllerError
+  end
+    rescue_from Exception,                        with: :rescue500
+    rescue_from ActiveRecord::RecordNotFound,     with: :rescue404
+    rescue_from ActionController::RoutingError,   with: :rescue404
+  
+  def routing_error
+    raise ActionController::RoutingError, params[:path]
+  end
+
 private
   
   def current_user
@@ -10,6 +20,14 @@ private
 
   def login_required
     redirect_to login_path unless current_user
+  end
+
+  def rescue404
+    render template: 'errors/error_404', status: 404
+  end
+
+  def rescue500
+    render template: 'errors/error_500', status: 500
   end
 
 end
